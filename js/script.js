81d8a2b7ec95e219bfc3af453b47315871f66d50
darkwinTech <s202257160@kfupm.edu.sa>
2025-09-18 17:57:13 +0300

    pushing assignment 1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Basic starter JS
 document.addEventListener('DOMContentLoaded', () => {
   const btn = document.getElementById('helloBtn');
-  if (btn) btn.addEventListener('click', () => alert('Hello from assignment-1!'));
+  if (btn) btn.addEventListener('click',  () => alert('Hello from assignment-1!'));
 });
