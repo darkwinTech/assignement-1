0d4f719b42c1d2e0e341f8aec9ddbb45141b4a12
darkwinTech <s202257160@kfupm.edu.sa>
2025-09-19 13:45:07 +0300

    Assignment 1: projects & contact

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 // Basic starter JS
-document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.getElementById('helloBtn');
-  if (btn) btn.addEventListener('click',  () => alert('Hello from assignment-1!'));
-});
+
+    var tablinks = document.getElementsByClassName('tab-links');
+    var taccontents = document.getElementsByClassName('tab-contents');
+    function openTab(tabname) {
+    for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+}
+    for (tabcontent of taccontents) {
+    tabcontent.classList.remove("active-tab");
+}
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
